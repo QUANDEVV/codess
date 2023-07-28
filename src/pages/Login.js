8f0076608e23bc371b860176f87abdddc73a1671
QUANDEVV <vercelploy@gmail.com>
2023-07-28 10:17:35 +0300

    Done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import { charity } from '../assets';
+import Typed from 'react-typed';
 
 const Login = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -59,32 +60,32 @@ const Login = () => {
         return (
           <form>
             {/* Donor Login Form */}
-            <div className="mb-4">
+            <div className="mb-4 font-epilogue">
               <label htmlFor="donor-username" className="block font-medium mb-1" >
                 Donor Username:
               </label>
               <input
             type='text'
             placeholder='Username'
-            className='bg-gray-600 text-white px-4 py-2 rounded'
+            className='bg-gray-200 text-white px-4 py-2 rounded'
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
             </div>
-            <div className="mb-4">
+            <div className="mb-4 font-epilogue">
               <label htmlFor="donor-password" className="block font-medium mb-1">
                 Donor Password:
               </label>
               <input
             type='password'
             placeholder='Password'
-            className='bg-gray-600 text-white px-4 py-2 rounded'
+            className='bg-gray-200 text-white px-4 py-2 rounded'
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
             </div>
-            <button type="submit" className="bg-blue-500 text-white rounded px-4 py-2" onClick={handleLogin}>
-              Login as Donor
+            <button type="submit"  className='bg-[#82cfb7] hover:bg-green-200 px-4 py-2 rounded' onClick={handleLogin}>
+              Login as donor
             </button>
           </form>
         );
@@ -92,35 +93,35 @@ const Login = () => {
         return (
           <form>
             {/* Charity Login Form */}
-            <div className="mb-4">
+            <div className="mb-4 font-epilogue">
               <label htmlFor="charity-username" className="block font-medium mb-1">
                 Charity Username:
               </label>
               <input
             type='text'
             placeholder='Username'
-            className='bg-gray-600 text-white px-4 py-2 rounded'
+            className='bg-gray-200 text-white px-4 py-2 rounded'
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
             </div>
-            <div className="mb-4">
+            <div className="mb-4 font-epilogue">
               <label htmlFor="charity-password" className="block font-medium mb-1">
                 Charity Password:
               </label>
               <input
             type='password'
             placeholder='Password'
-            className='bg-gray-600 text-white px-4 py-2 rounded'
+            className='bg-gray-200 text-white px-4 py-2 rounded'
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
             </div>
             <button
-            className='bg-blue-500 hover:bg-blue-700 px-4 py-2 rounded'
+            className='bg-[#82cfb7] hover:bg-green-200 px-4 py-2 rounded'
             onClick={handleLogin}
           >
-            Login
+            Login as charity
           </button>
           </form>
         );
@@ -135,28 +136,29 @@ const Login = () => {
               <input
             type='text'
             placeholder='Username'
-            className='bg-gray-600 text-white px-4 py-2 rounded'
+            className='bg-gray-200 text-white px-4 py-2 rounded'
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
             </div>
             <div className="mb-4">
-              <label htmlFor="admin-password" className="block font-medium mb-1">
+              <label htmlFor="admin-password" className="font-epilogue block font-medium mb-1">
                 Admin Password:
               </label>
               <input
             type='password'
             placeholder='Password'
-            className='bg-gray-600 text-white px-4 py-2 rounded'
+            className='bg-gray-200 text-white px-4 py-2 rounded'
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
             </div>
-            <button type="submit" className="bg-blue-500 text-white rounded px-4 py-2" onClick={handleLogin}>
+            <button type="submit"  className='bg-[#82cfb7] hover:bg-green-200 px-4 py-2 rounded' onClick={handleLogin}>
               Login as Admin
             </button>
           </form>
         );
+        
       default:
         return null;
     }
@@ -166,33 +168,38 @@ const Login = () => {
   return (
     <div className="flex flex-col min-h-screen">
       {/* Navbar */}
-      <div className="bg-blue-500 p-4 text-white flex justify-end">
-        <div className="cursor-pointer" onClick={handleLoginClick}>
+
+      <div className=" p-4 text-white flex justify-end gap-6 mt-4">
+      <h1 className='w-full text-4xl font-bold text-[#00df9a] mt-1' >Uplift.</h1>
+        <h1 className="cursor-pointer mt-3 bg-white text-black py-2 px-4 rounded-[10px]" onClick={handleLoginClick}>
           Login
-        </div>
+        </h1>
+        <h1 className="cursor-pointer mt-3 bg-white text-black py-2 px-2 rounded-[10px]" onClick={handleLoginClick}>
+          Signup
+        </h1>
       </div>
 
       {/* Modal */}
       {isModalOpen && (
-        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-75">
           <div className="bg-white p-4 rounded shadow-md">
             {/* Display login options */}
-            <h2 className="text-2xl font-semibold mb-4">Login</h2>
+            <h2 className="font-epilogue text-2xl font-semibold mb-4">Login</h2>
             <div className="grid gap-4 grid-cols-3">
               <div
-                className="cursor-pointer text-center border border-blue-500 p-2 rounded"
+                className="cursor-pointer  font-epilogue mb-2 text-center border border-green-700 p-2 rounded"
                 onClick={() => handleLoginOptionClick('donor')}
               >
                 Donor
               </div>
               <div
-                className="cursor-pointer text-center border border-blue-500 p-2 rounded"
+                className="cursor-pointer font-epilogue mb-2 text-center border border-green-700 p-2 rounded"
                 onClick={() => handleLoginOptionClick('charity')}
               >
                 Charity
               </div>
               <div
-                className="cursor-pointer text-center border border-blue-500 p-2 rounded"
+                className="cursor-pointer font-epilogue mb-2 text-center border border-green-700 p-2 rounded"
                 onClick={() => handleLoginOptionClick('admin')}
               >
                 Admin
@@ -201,7 +208,7 @@ const Login = () => {
 
             {/* Render the appropriate login form based on the selected user type */}
             {renderLoginForm()}
-            <button onClick={handleCloseModal} className="bg-red-500 text-white rounded px-4 py-2 mt-4">
+            <button onClick={handleCloseModal} className="bg-red-400 text-white rounded px-4 py-2 mt-4">
               Close
             </button>
           </div>
@@ -209,14 +216,24 @@ const Login = () => {
       )}
 
 
-      <div className="flex flex-grow items-center justify-center px-8">
+      <div className="w-full flex flex-grow items-center justify-center px-8 mb-20">
         <div className="flex flex-col items-center justify-center text-center w-1/2">
-          <h1 className="text-6xl font-bold text-blue-500">Charity</h1>
+          <h1 className="text-6xl font-bold text-[#00df9a]">Charity</h1>
           <p className="text-xl mt-4 text-white">Join us in making a difference.</p>
+          <Typed 
+          className='text-white'
+          strings={['We are a non-profit organization that aims to help those in need.']}
+          typeSpeed={120}
+          backSpeed={140}
+          loop
+          
+          
+          />
+
         </div>
 
         <div className="w-1/2">
-          <img src={charity} alt="Charity" className="w-full h-full object-cover" />
+          <img src={charity} alt="Charity" className="w-full h-full object-cover rounded-[16px]" />
         </div>
       </div>
     </div>
